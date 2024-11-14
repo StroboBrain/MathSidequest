3f134bcbf8096ab33d64f3d06a4dec06df9161c4
StroboBrain <parkschaden@gmail.com>
2024-11-14 16:47:50 +0100

    minimum outline for libary

--- scripts/mathLibary/task.js
+++ scripts/mathLibary/task.js
@@ -1,3 +1,19 @@
 /**
- * Task object to hold a task
+ * Task object to hold a task, has no functionality in creating a task
  */
+
+
+class Task {
+
+    constructor(solution, taskAsString) {
+        this.name = solution = solution;
+        this.taskAsString = taskAsString;
+    }
+
+    logTask() {
+        console.log(`Task: ${this.name}, Description: ${this.description}`);
+    }
+}
+
+// Named export
+export { Task };
--- scripts/mathLibary/taskDisplay.js
+++ scripts/mathLibary/taskDisplay.js
@@ -1,3 +1,27 @@
 /**
- * Displays a Task
+ * Displays the tasks, only one object is needed.
  */
+
+import Task from './task.js';
+
+
+class TaskDisplay {
+
+    constructor() {
+        this.taskArray = [];
+    }
+
+    addTask(task){
+        this.taskArray.push(task);
+    }
+
+    displayTask(elementID){
+        // Displays the current at elementID object
+        
+    }
+
+
+}
+
+// Named export
+export { TaskDisplay };
