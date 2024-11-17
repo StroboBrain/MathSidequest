4de7257d5c8a53030b41f44827729acfc93e2c18
StroboBrain <parkschaden@gmail.com>
2024-11-17 20:31:44 +0100

    working on math libary

--- scripts/mathLibary/task.js
+++ scripts/mathLibary/task.js
@@ -6,14 +6,32 @@
 class Task {
 
     constructor(solution, taskAsString) {
-        this.name = solution = solution;
+        this.solution = solution;
         this.taskAsString = taskAsString;
     }
 
     logTask() {
         console.log(`Task: ${this.name}, Description: ${this.description}`);
     }
+
+    // check if a solution is valid for a specific task
+    confirmSolution(input){
+        return this.solution == input;
+    }
+}
+
+// This class holds a list of task
+class TaskList {
+
+    constructor(){
+        this.taskList = [];
+    }
+    addTask(task){
+        this.taskList.append(task)
+    }
+
 }
 
 // Named export
 export { Task };
+export {TaskList};
---
+++ scripts/mathLibary/taskCreator.js
@@ -0,0 +1,12 @@
+/**
+ *  Creates a taskList
+ */
+
+import * as task from './task.js';
+
+class taskCreator {
+    constructor (taskType, numberOfTasks){
+        this.numberOfTasks = numberOfTasks;
+        this.taskTyp = taskType;
+    }
+}
